6488a88580bd4e766d43cca0d48fa792fc9f03d0
TUng Hoang <hoangtung240603@gmail.com>
2022-10-31 21:05:38 +0700

    10 commit

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 
 
+var filmNew = [
+    {
+        name: 'chúa tể của những chiếc nhẫn',
+        public: '2010'
+    },
+    {
+        name: 'harry poster',
+        public: '2009'
+    },
+    {
+        name: 'docter strange',
+        public: '2012'
+    }
+
+
+];
+
+var film = filmNew.map(function(){
+    console.log(filmNew)
+});
+
+
